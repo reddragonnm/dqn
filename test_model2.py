e939c003d91ec1fc2e22d7f66bb6ef025a519600
reddragonnm <neeleshojmishra@gmail.com>
2025-10-21 13:25:24 +0530

    Complete experiment 2

--- test_model2.py
+++ test_model2.py
@@ -1,25 +1,27 @@
 import torch
 from torch import nn
 import gymnasium as gym
+from gymnasium.wrappers import AtariPreprocessing
+
 import ale_py
 import cv2
 from matplotlib import pyplot as plt
 from collections import deque
-import random
-import copy
 
 gym.register_envs(ale_py)
 env = gym.make("ALE/Breakout-v5", render_mode="human")
+env = AtariPreprocessing(
+    env, grayscale_obs=True, scale_obs=True, frame_skip=1, terminal_on_life_loss=True
+)
 
 
 def preprocess_image(image, crop_top=20):
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) / 255.0
     image = cv2.resize(image, (84, 110))
-    return image[crop_top : crop_top + 84, :]  # 84x84 grayscale
+    return image[crop_top : crop_top + 84, :]
 
 
 class DQN(nn.Module):
-    def __init__(self, num_actions=4):  # inp shape: (b, 4, 84, 84)
+    def __init__(self, num_actions=3):  # inp shape: (b, 4, 84, 84)
         super().__init__()
 
         self.layers = nn.Sequential(
@@ -39,37 +41,32 @@ class DQN(nn.Module):
 
 model = DQN().to("cpu")
 model.load_state_dict(
-    torch.load("models/dqn_model_episode_4380.pth", map_location="cpu")
+    torch.load("models2/dqn_model2_episode_7500.pth", map_location="cpu")
 )
 
 model.eval()
 
-for episode in range(10):
-    episode_reward = 0
-    episode_frames = 0
+ACTION_MAP = [0, 2, 3]  # NOOP, RIGHT, LEFT
 
+for episode in range(10):
     obs, info = env.reset()
+    env.step(1)  # FIRE
+    env.step(1)  # FIRE
+
     frame_stack = deque([preprocess_image(obs)] * 4, maxlen=4)
 
     done = False
 
     while not done:
-        if episode_frames % 100 == 0:
-            action = 1
-            print("FIRE!")
-        else:
-            with torch.no_grad():
-                inp = torch.tensor(frame_stack, dtype=torch.float32).unsqueeze(
-                    0
-                )  # add batch dim
-                action = torch.argmax(model(inp), dim=1).item()
+        with torch.no_grad():
+            inp = torch.tensor(frame_stack, dtype=torch.float32).unsqueeze(
+                0
+            )  # add batch dim
+            action_idx = torch.argmax(model(inp), dim=1).item()
 
+        action = ACTION_MAP[action_idx]
         obs, reward, terminated, truncated, info = env.step(action)
 
-        # print(f"Selected action: {action} info: {info}")
-
-        episode_reward += reward
-        episode_frames += 1
         done = terminated or truncated
 
         frame_stack.append(preprocess_image(obs))
